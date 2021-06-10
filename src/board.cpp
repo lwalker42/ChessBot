5cb1c56e16a451244c544b234daf5d0f00d3ba6e
Liam Walker <lrwalker@andrew.cmu.edu>
2021-06-10 17:46:54 -0400

    move_piece overloading

--- src/board.cpp
+++ src/board.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 
 #include "piece.hpp"
+#include "pos.hpp"
 #include "move.hpp"
+#include "pos_move.hpp"
 #include "board_constants.hpp"
 #include "board.hpp"
 #include "util.hpp"
@@ -51,6 +53,18 @@ piece_t Board::move_piece(int r, int c, Move m) {
     return move_piece(r, c, r + m.r, c + m.c);
 }
 
+piece_t Board::move_piece(Pos p, int r, int c) {
+    return move_piece(p.r, p.c, p.r + r, p.c + c);
+}
+
+piece_t Board::move_piece(Pos p, Move m) {
+    return move_piece(p.r, p.c, p.r + m.r, p.c + m.c);
+}
+
+piece_t Board::move_piece(Pos_Move pm) {
+    return move_piece(pm.pos, pm.move);
+}
+
 
 
 moves_t Board::get_moves(int r, int c) const{
@@ -59,13 +73,17 @@ moves_t Board::get_moves(int r, int c) const{
     return moves;
 }
 
+moves_t Board::get_moves(Pos p) const {
+    return get_moves(p.r, p.c);
+}
+
 moves2_t Board::get_moves_lists(int r, int c) const {
     if (!on_board(r, c)) return {};
     return get_piece_moves(board[r][c]);
 }
 
 moves_t Board::filter_moves_lists(int r, int c, moves2_t moves_list) const {
-    std::cout << "Starting with move list: " << move::to_string(moves_list);
+    //std::cout << "Starting with move list: " << move::to_string(moves_list);
 
     if (!on_board(r, c)) return {};
     piece_t p1 = board[r][c];
@@ -80,18 +98,18 @@ moves_t Board::filter_moves_lists(int r, int c, moves2_t moves_list) const {
             if(on_board(r_move, c_move)) {
                 piece_t p2 = board[r_move][c_move];
                 if(p2 == __) {                          //piece is empty
-                    std::cout << "Empty space\n";
+                    //std::cout << "Empty space\n";
                     continue;
                 } else if(same_color(p1, p2)) {         //piece color of location is the same
-                    std::cout << "Friendly piece\n";
+                    //std::cout << "Friendly piece\n";
                     break;
                 } else {                                //piece is opposite color: capture (i.e. increment iterator then break)
-                    std::cout << "Enemy piece\n";
+                    //std::cout << "Enemy piece\n";
                     it++;
                     break;
                 }
             } else {                                    //out of bounds
-                std::cout << "Out of bounds\n";
+                //std::cout << "Out of bounds\n";
                 break;
             }
         }
--- src/board.hpp
+++ src/board.hpp
@@ -5,6 +5,8 @@
 
 #include "board_constants.hpp"
 #include "move.hpp"
+#include "pos.hpp"
+#include "pos_move.hpp"
 #include "piece.hpp"
 
 class Board {
@@ -18,8 +20,12 @@ class Board {
         board_t get_board();
         piece_t move_piece(int, int, int = -1, int = -1);
         piece_t move_piece(int, int, Move);
+        piece_t move_piece(Pos, int = -1, int = -1);
+        piece_t move_piece(Pos, Move);
+        piece_t move_piece(Pos_Move);
 
         moves_t get_moves(int, int) const;
+        moves_t get_moves(Pos) const;
         moves2_t get_moves_lists(int, int) const;
         moves_t filter_moves_lists(int, int, moves2_t) const;
         moves_t filter_check(int, int, moves_t) const;
