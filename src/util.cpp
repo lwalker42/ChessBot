a2430dbbe752a7619dd2ca6d526068138abd7462
Liam Walker <lrwalker@andrew.cmu.edu>
2021-06-10 17:51:20 -0400

    on_board overloading

--- src/util.cpp
+++ src/util.cpp
@@ -1,6 +1,11 @@
-#include "board.hpp"
+#include "board_constants.hpp"
 #include "util.hpp"
+#include "pos.hpp"
 
 bool on_board(int r, int c) {
     return (0 <= r && r < BOARD_SIZE && 0 <= c && c < BOARD_SIZE);
+}
+
+bool on_board(Pos p) {
+    return on_board(p.r, p.c);
 }
--- src/util.hpp
+++ src/util.hpp
@@ -1,7 +1,9 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
+#include "pos.hpp"
 
 bool on_board(int, int);
+bool on_board(Pos);
 
 #endif
